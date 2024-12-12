26c39ace5c9850734154dcb6a528e505e5fa3b31
Ximo Llacer Bernabeu <xllaber7@gmail.com>
2024-12-12 11:19:05 +0100

    material design setup

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
